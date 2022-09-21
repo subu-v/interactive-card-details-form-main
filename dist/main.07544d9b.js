5aeeaba7d82705717c7cccfee86c71e7b46f4c0b
subramani <106590803+subu-v@users.noreply.github.com>
2022-09-21 19:11:43 +0530

    Done for today, coded 3hrs and 17mins
    Had problem positioning the images and the content that should be placed inside the images

--- dist/main.07544d9b.js
+++ dist/main.07544d9b.js
@@ -189,7 +189,7 @@ var reloadCSS = require('_css_loader');
 
 module.hot.dispose(reloadCSS);
 module.hot.accept(reloadCSS);
-},{"_css_loader":"../node_modules/parcel-bundler/src/builtins/css-loader.js"}],"../node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+},{"./..\\..\\images\\bg-main-desktop.png":[["bg-main-desktop.069dc9f9.png","../images/bg-main-desktop.png"],"../images/bg-main-desktop.png"],"_css_loader":"../node_modules/parcel-bundler/src/builtins/css-loader.js"}],"../node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 var OldModule = module.bundle.Module;
